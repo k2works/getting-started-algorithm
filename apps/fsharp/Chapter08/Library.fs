a532df88124dd829f3c28883d359eacc86a64f72
k2works <kakimomokuri@gmail.com>
2026-04-13 09:51:22 +0900

    refactor(fsharp): ディレクトリ構成を C# 版に合わせて単一プロジェクトに再編する
    Chapter01〜09 の 18 プロジェクト構成から
    Algorithm / Algorithm.Tests の 2 プロジェクト構成に変更。
    全 238 テストは引き続き合格。

--- apps/fsharp/Chapter08/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Chapter08
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- apps/fsharp/Chapter09/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Chapter09
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
